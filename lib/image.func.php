d8ee41db134decaa7f969d295e315cd264ab0af1
MarcusDexter <a@cys.pw>
2015-09-24 16:08:50 +0800

    修改验证码尺寸
    改为120x50

--- lib/image.func.php
+++ lib/image.func.php
@@ -15,8 +15,8 @@ require_once 'string.func.php';
 function verifyImage($type=1,$length=4,$pixel=1,$line=1,$session_name = "verify")
 {
     //session_start();
-    $width = 80;
-    $height = 40;
+    $width = 120;
+    $height = 50;
     $image = imagecreatetruecolor($width, $height);
     $white = imagecolorallocate($image, 255, 255, 255);
     $black = imagecolorallocate($image, 0, 0, 0);
@@ -26,12 +26,12 @@ function verifyImage($type=1,$length=4,$pixel=1,$line=1,$session_name = "verify"
     $_SESSION[$session_name] = $chars;
     $fontfiles = array("ARLRDBD.TTF", "BRLNSR.TTF", "ITCKRIST.TTF");
     for ($i = 0; $i < $length; $i++) {
-        $size = mt_rand(14, 18);
+        $size = mt_rand(20, 30);
         $angle = mt_rand(-15, 15);
-        $x = 5 + $i * $size;
-        $y = mt_rand(20, 30);
+        $x = 15 + $i * $size;
+        $y = mt_rand(20, 50);
         $color = imagecolorallocate($image, mt_rand(50, 180), mt_rand(50, 180), mt_rand(50, 180));
-        $fontfile = "../fonts/" . $fontfiles[mt_rand(0, count($fontfiles)-1)];
+        $fontfile = "./fonts/" . $fontfiles[mt_rand(0, count($fontfiles)-1)];
         $text = substr($chars, $i, 1);
         imagettftext($image, $size, $angle, $x, $y, $color, $fontfile, $text);
     }
