f1a769f3659072489a1fe35e67bb355fb723c74f
MarcusDexter <a@cys.pw>
2015-11-07 17:42:18 +0800

    分类管理服务端
    接收操作及数据并返回数据

--- admin/category.server.php
+++ admin/category.server.php
@@ -2,5 +2,15 @@
 require_once '../include.php';
 header("Content-Type: application/json;charset=utf-8");
 $myCategory=new category();
+if(isset($_POST['act'])){
+    if(isset($_POST['id']))$myCategory->setId($_POST['id']);
+    if(isset($_POST['categoryName']))$myCategory->setCategoryName($_POST['categoryName']);
+    if(isset($_POST['CName']))$myCategory->setCName($_POST['CName']);
+    switch($_POST['act']){
+        case 'create': $myCategory->insertCategory();break;
+        case 'update': $myCategory->updateCategory();break;
+        case 'delete': $myCategory->deleteCategory();break;
+    }
+}
 $arr=$myCategory->getAllCategory();
 echo json_encode($arr);
